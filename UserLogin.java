0941baa8b652e9bbfc9b241bcad13bb64ee537dd
sonu <anjs.telang@gmail.com>
2023-01-26 20:10:13 +0530

    UserLogin class created

---
+++ UserLogin.java
@@ -0,0 +1,27 @@
+package com.velocity.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class UserLogin {
+    public UserLogin getUserAccess(String UserName, String Password) {
+    	UserLogin userLogin = new UserLogin();
+    	
+    	Connection con = null;
+    	PreparedStatement ps= null;
+    	try {
+    		ConnectionProvider connection = new ConnectionProvider();
+    		con = connection.getConnectionDetails();
+    		ps =con.prepareStatement("insert into user(UserName, Password) values(?,?)");
+    		ps.setString(1, UserName);
+    		ps.setString(2, Password);
+    		ps.executeUpdate();
+    		System.out.println("Successfully Logged In...");
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} 
+    	return userLogin;
+    }
+	
+
+}
