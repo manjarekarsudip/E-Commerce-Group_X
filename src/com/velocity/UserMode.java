bc24cf35f5705d32de65dd24f2697236cd06b0ff
sonaliwale <sonaliwale2016@gmail.com>
2023-01-27 20:44:17 +0530

    User Mode and Registered List added

---
+++ src/com/velocity/UserMode.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class UserMode {
+
+	public void getUserAccess() {
+		UserMode um=new UserMode();
+		UserRegistration ur=new UserRegistration();
+		ur.userDetail();
+		
+	}
+
+	
+
+}
